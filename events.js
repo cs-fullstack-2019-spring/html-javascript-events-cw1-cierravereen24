c1ed27c4aa6c1cb09f147dc0c89172a4fdc1862e
cierravereen24 <cierravereen24@gmail.com>
2019-02-04 16:55:42 -0600

    cw

--- events.js
+++ events.js
@@ -1,3 +1,6 @@
+
+// # HTML frontend JavaScript events CW
+
 console.log("hghug");
 var idx =0;
 var myText = "Cierra Vereen is a queen amongst a world full of peasants. Her every action is grace and elegance personified. The beauty that the Queen Cierra possesses can not be put into word. Most would imagine that a queen of such caliber would be full of themselves but not Queen Cierra. She is the most humble, caring, and just queen that has sat on the throne.";
